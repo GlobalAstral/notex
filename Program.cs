d62a306aaab4f6f5729ba8c2ce61510877684689
GlobalAstral <cristianmainenti.03@gmail.com>
2026-09-21 14:55:09 +0200

    Started development

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Markdig;
+using Markdig.Syntax;
+
+string filepath = (string?)args.GetValue(0) ?? throw new ArgumentException("Command line argument expected: filename");
+
+if (Path.GetExtension(filepath) != ".md")
+  throw new ArgumentException("Filename is not a .md file");
+
+string texfile = Path.ChangeExtension(filepath, ".tex");
+
+string markdown = File.ReadAllText(filepath);
+
+var pipeline = new MarkdownPipelineBuilder()
+  .UseAdvancedExtensions()
+  .Build();
+
+MarkdownDocument document = Markdown.Parse(markdown, pipeline);
