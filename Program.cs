a0fa20f18cd51cc009fed7f3bebe0debbc6dcba7
GlobalAstral <cristianmainenti.03@gmail.com>
2026-09-24 20:22:02 +0200

    Made program more flexible

--- Program.cs
+++ Program.cs
@@ -4,33 +4,44 @@ using compiler;
 using Markdig;
 using Markdig.Syntax;
 
-bool debug = args.Contains("--debug");
-
-string filepath = debug ? "test.md" : (string?)args.GetValue(0) ?? throw new ArgumentException("Command line argument expected: filename");
-
-if (Path.GetExtension(filepath) != ".md")
-  throw new ArgumentException("Filename is not a .md file");
-
-string texfile = Path.ChangeExtension(filepath, ".tex");
-
-string markdown = File.ReadAllText(filepath);
-
-var pipeline = new MarkdownPipelineBuilder()
-  .UseAdvancedExtensions()
-  .UseYamlFrontMatter()
-  .UseSmartyPants()
-  .Build();
-
-MarkdownDocument document = Markdown.Parse(markdown, pipeline);
-
-if (debug)
+static void Process(string filepath, bool debug)
 {
-  foreach (var block in document)
-    Console.WriteLine(block.GetType().FullName);
+  if (Path.GetExtension(filepath) != ".md")
+    throw new ArgumentException("Filename is not a .md file");
+
+  string texfile = Path.ChangeExtension(filepath, ".tex");
+
+  string markdown = File.ReadAllText(filepath);
+
+  var pipeline = new MarkdownPipelineBuilder()
+    .UseAdvancedExtensions()
+    .UseYamlFrontMatter()
+    .UseSmartyPants()
+    .Build();
+
+  MarkdownDocument document = Markdown.Parse(markdown, pipeline);
+
+  if (debug)
+  {
+    foreach (var block in document)
+      Console.WriteLine(block.GetType().FullName);
+  }
+
+  IHolder holder = new Converter(document).Convert();
+
+  string tex = Compiler.Compile(holder);
+
+  File.WriteAllText(texfile, tex);
 }
 
-IHolder holder = new Converter(document).Convert();
+List<string> arguments = [.. args];
 
-string tex = Compiler.Compile(holder);
+bool debug = arguments.Remove("--debug");
 
-File.WriteAllText(texfile, tex);
+string[] files = [ .. args ];
+
+if (files.Length < 1)
+  throw new ArgumentException("Command line argument expected: filename");
+
+foreach (string filepath in arguments)
+  Process(filepath, debug);
